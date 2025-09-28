6fcc7e48fbfb04e15d5ca27f1e949e6a9ad5a80f
Gianluca Corsini <gianluca.corsini@irisa.fr>
2025-09-28 23:36:14 +0200

    Modify scripts to log event time and provide job duration estimation
    Now, most of the scripts add to a logfile the (machine) time (in secs)
    at which the event occurs.
    The last script appending to it, namely 'print_end_job.py', will parse
    it and computes a job duration estimation from the time of each event.

--- python/print_resume_job.py
+++ python/print_resume_job.py
@@ -1,8 +1,11 @@
-import serial
 import datetime
+import math
+import serial
+import time
 
 port = '/dev/serial0'
 baudrate = 9600
+filename = '.logtime'
 
 ser = serial.Serial(port=port, baudrate=baudrate)
 
@@ -10,3 +13,8 @@ now = datetime.datetime.now()
 ser.write(bytes(f'{now.strftime("%d/%m/%Y | %H:%M:%S")} | Riprendi\n', 'utf-8'))
 ser.flush()
 
+"""log resume time"""
+t = math.trunc(time.clock_gettime(time.CLOCK_REALTIME))
+with open(filename, 'a') as f:
+    f.write(f'resume:{str(t)}\n')
+    f.close()
