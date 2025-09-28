8df782873d763a44f36a00031d1eca7653701d56
Gianluca Corsini <gianluca.corsini@irisa.fr>
2025-09-28 23:10:32 +0200

    Improve example and scripts
    * Introduce variables for port and baudrate.
    * Replace string concatenation with f-strings.
    * Flush after writing to serial port.

--- python/print_resume_job.py
+++ python/print_resume_job.py
@@ -1,8 +1,12 @@
 import serial
 import datetime
 
-ser = serial.Serial(port='/dev/serial0', baudrate=9600)
+port = '/dev/serial0'
+baudrate = 9600
+
+ser = serial.Serial(port=port, baudrate=baudrate)
 
 now = datetime.datetime.now()
-ser.write(bytes(now.strftime("%d/%m/%Y | %H:%M:%S")+' | Riprendi\n', 'utf-8'))
+ser.write(bytes(f'{now.strftime("%d/%m/%Y | %H:%M:%S")} | Riprendi\n', 'utf-8'))
+ser.flush()
 
