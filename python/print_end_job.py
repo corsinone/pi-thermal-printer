c4b58f58209b43632d7fbd0c936d1622c421e3cb
Gianluca Corsini <gianluca.corsini@irisa.fr>
2025-09-28 23:36:14 +0200

    Modify scripts to log event time and provide job duration estimation
    Now, most of the scripts add to a logfile the (machine) time (in secs)
    at which the event occurs.
    The last script appending to it, namely 'print_end_job.py', will parse
    it and computes a job duration estimation from the time of each event.

--- python/print_end_job.py
+++ python/print_end_job.py
@@ -1,11 +1,50 @@
-import serial
 import datetime
+import math
+import serial
+import time
 
 port = '/dev/serial0'
 baudrate = 9600
+filename = '.logtime'
 
 ser = serial.Serial(port=port, baudrate=baudrate)
 
+"""log end time"""
+t = math.trunc(time.clock_gettime(time.CLOCK_REALTIME))
+with open(filename, 'a') as f:
+    f.write(f'end:{str(t)}')
+    f.close()
+
+"""parse time log file to estimate job duration"""
+with open(filename, 'r') as f:
+    state = 0
+    duration = 0
+    for line in f:
+        result = str.split(line, sep=":")
+        if state == 0 and result[0] == 'start':
+            duration -= int(result[1])
+            state = 1
+        elif state == 1 and (result[0] == 'pause' or result[0] == 'end'):
+            duration += int(result[1])
+            state = 2
+        elif state == 2 and result[0] == 'resume':
+            duration -= int(result[1])
+            state = 1
+        else:
+            print('time log file is inconsistent', file=sys.stderr)
+            exit()
+
+    """compute duration as days, hours, mins, secs"""
+    result = math.modf(duration/60)
+    secs = math.ceil(result[0]*60)
+    result = math.modf(int(result[1])/60)
+    mins = math.ceil(result[0])*60
+    result = math.modf(int(result[1])/24)
+    hours = math.ceil(result[0]*24)
+    days = int(result[1])
+    f.close()
+
 now = datetime.datetime.now()
 ser.write(bytes(f'{now.strftime("%d/%m/%Y | %H:%M:%S")} | Fine\n', 'utf-8'))
+ser.write(bytes(f'{days}d {hours}h {mins}m {secs}s\n', 'utf-8'))
 ser.flush()
--- python/print_start_job.py
+++ python/print_start_job.py
@@ -1,8 +1,11 @@
-import serial
 import datetime
+import math
+import serial
+import time
 
 port = '/dev/serial0'
 baudrate = 9600
+filename = '.logtime'
 
 ser = serial.Serial(port=port, baudrate=baudrate)
 
@@ -10,3 +13,8 @@ now = datetime.datetime.now()
 ser.write(bytes(f'{now.strftime("%d/%m/%Y | %H:%M:%S")} | Inizio\n', 'utf-8'))
 ser.flush()
 
+"""log start time"""
+t = math.trunc(time.clock_gettime(time.CLOCK_REALTIME))
+with open(filename, 'w') as f:
+    f.write(f'start:{str(t)}\n')
+    f.close()
