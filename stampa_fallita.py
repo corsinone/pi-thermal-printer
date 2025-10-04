1c81847b0c93d593315315f8fd8d89b4ac059893
Giorgio Corsini <corsinigiorgio61@gmail.com>
2025-10-04 19:05:53 +0200

    Modifica formato anno da 4 cifre a 2 cifre
    Esempio invece di stampare 2025 stamperà 25.

--- stampa_fallita.py
+++ stampa_fallita.py
@@ -2,4 +2,4 @@ import serial
 import datetime
 ser = serial.Serial(port='/dev/serial0', baudrate=9600)
 now = datetime.datetime.now()
-ser.write(bytes(now.strftime("%d/%m/%Y %H:%M:%S")+' Fallita\n', 'utf-8'))
+ser.write(bytes(now.strftime("%d/%m/%y %H:%M:%S")+' Fallita\n', 'utf-8'))
--- stampa_finita.py
+++ stampa_finita.py
@@ -2,7 +2,7 @@ import serial
 import datetime
 ser = serial.Serial(port='/dev/serial0', baudrate=9600)
 now = datetime.datetime.now()
-ser.write(bytes(now.strftime("%d/%m/%Y %H:%M:%S")+' Fine stampa\n', 'utf-8'))
-ser.write(bytes(now.strftime("%d/%m/%Y %H:%M:%S")+' OCTO FINE\n', 'utf-8'))
+ser.write(bytes(now.strftime("%d/%m/%y %H:%M:%S")+' Fine stampa\n', 'utf-8'))
+ser.write(bytes(now.strftime("%d/%m/%y %H:%M:%S")+' OCTO FINE\n', 'utf-8'))
 ser.write(bytes('__________FINE LAVORO_________\n', 'utf-8'))
 ser.write(bytes('\n\n\n','utf-8'))
